0cddb7a1629b98ef96637bea997d10b470883ea8
Vedant Nandwana <vedantnn7.git@gmail.com>
2022-07-25 14:47:25 +0530

    Implement(nav): for temporary use case

--- apps/webapp/next.config.js
+++ apps/webapp/next.config.js
@@ -1,5 +1,8 @@
 const withTM = require("next-transpile-modules")(["ui", "lib"]);
 
 module.exports = withTM({
+  images: {
+    domains: ["lh3.googleusercontent.com"]
+  },
   reactStrictMode: true,
 });
