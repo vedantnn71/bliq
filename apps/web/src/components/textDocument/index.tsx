a0b18bf521c3962abd8d2529ab35bd4dad43777e
Vedant Nandwana <vedantnn7.git@gmail.com>
2022-07-26 16:33:34 +0530

    Update(web/textDocument): save content to store after every 3 seconds

--- apps/web/src/components/textDocument/index.tsx
+++ apps/web/src/components/textDocument/index.tsx
@@ -2,15 +2,30 @@ import type { ChangeEvent } from "react";
 import { SimpleGrid, Flex, Box, Heading, Textarea } from "@chakra-ui/react";
 import { useState, useEffect } from "react";
 import { useEditor, EditorContent } from "@tiptap/react";
+import { useDocumentStore } from "store";
 import StarterKit from "@tiptap/starter-kit";
 import Menubar from "./menubar";
 import styles from "styles/document.module.css";
 
 const TextDocument = () => {
+  const content = useDocumentStore(state => state.content);
+  const setContent = useDocumentStore(state => state.setContent);
+
   const editor = useEditor({
     extensions: [StarterKit],
-    content: "<p style='color: rgba(0, 0, 0, 0.9)'>Start writing...</p>",
+    content
   });
+
+  useEffect(() => {
+    const saveTimer = setTimeout(() => {
+      setContent({
+        content: editor.getHTML(),
+      });
+    }, 5000);
+
+    return () => clearTimeout(saveTimer);
+
+  }, [editor, content, setContent])
 
   return (
     <Flex h="100%" direction="column" justify="space-between">
