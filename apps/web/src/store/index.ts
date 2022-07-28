17a3b08f53063d1d45fbf9499717456a36a540b0
Vedant Nandwana <vedantnn7.git@gmail.com>
2022-07-28 15:34:33 +0530

    Update(web/store): Implement fetch

--- apps/web/src/store/index.ts
+++ apps/web/src/store/index.ts
@@ -1,14 +1,16 @@
 import create from "zustand";
 import { devtools, persist } from "zustand/middleware";
+import { ObjectId } from "mongodb";
+import axios from "axios";
 
 interface DocumentStore {
   name: string;
   content: string;
   completion: string;
-  id: string;
-  setId: (id: string) => void;
+  id?: ObjectId;
   setName: (name: string) => void;
   setContent: (content: string) => void;
+  fetch: Promise<id: ObjectId> => void;
 }
 
 const useDocumentStore = create<DocumentStore>()(
@@ -17,10 +19,18 @@ const useDocumentStore = create<DocumentStore>()(
       name: "Untitled",
       content: "<p>Start writing ...</p>",
       completion: "",
-      id: "",
-      setId: (id: string) => set((state) => ({ id })),
       setName: (name: string) => set((state) => ({ name })),
       setContent: (content: string) => set((state) => ({ content })),
+      fetch: async (id: ObjectId) => {
+        const response = await axios.get(`/api/document/${id}`);
+        const data = await response.data;
+
+        set({
+          id: new ObjectId(data?._id as string),
+          name: data?.name as string,
+          content: data?.content as string
+        });
+      },
     }))
   )
 );
