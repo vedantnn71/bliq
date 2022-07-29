3521ea647ab53d1542f209a56195232755a42501
Vedant Nandwana <vedantnn7.git@gmail.com>
2022-07-29 21:04:11 +0530

    Update(web/index): show documents rather than a text editor

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -1,25 +1,50 @@
 import type { NextPage } from "next";
+import type { ObjectId } from "bson";
 import Head from "next/head";
 import Image from "next/image";
 import Nav from "components/nav";
-import Header from "components/textDocument/header";
-import TextDocument from "components/textDocument";
+import DocumentCard from "components/documentCard";
+import axios from "axios";
 import { Flex } from "@chakra-ui/react";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
+import { useQuery } from "@tanstack/react-query";
+import generateKey from "lib/react/generateKey";
+
+const getDocuments = async () => {
+  const response = await axios.get("/api/document");
+  return response.data;  
+}
+
+type Document = {
+  name?: string;
+  content?: string;
+  _id?: ObjectId;
+}
 
 const Home: NextPage = () => {
   const { status } = useSession();
+  const { data, isLoading } = useQuery(["/api/document"], getDocuments);
   const router = useRouter();
 
   if (status === "unauthenticated") router.push("/auth/signup");
 
+  if (isLoading) return <h1>loading...</h1>;
+
   return (
     <Flex h="100vh" direction="row">
       <Nav />
-      <Flex minH="100%" w="100%" direction="column">
-        <Header />
-        <TextDocument />
+      <Flex direction="row" wrap="wrap" gap="8" p="8">
+        <DocumentCard variant="new" />
+        {data.map((document: Document) => (
+          <DocumentCard
+            variant="card"
+            name={document?.name}
+            content={document?.content}
+            documentId={document?._id}
+            key={generateKey(Math.floor(Math.random()*10000))}
+          />
+        ))}
       </Flex>
     </Flex>
   );
