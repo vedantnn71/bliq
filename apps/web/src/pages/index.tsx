24d713d9b286c96189505eab311e97fd97e4fa7f
Vedant Nandwana <vedantnn7.git@gmail.com>
2022-07-30 15:25:30 +0530

    Update(web/index): implement search

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -4,17 +4,17 @@ import Head from "next/head";
 import Image from "next/image";
 import Nav from "components/nav";
 import DocumentCard from "components/documentCard";
+import Header from "components/header";
 import axios from "axios";
+import autoAnimate from '@formkit/auto-animate'
 import { Flex } from "@chakra-ui/react";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 import { useQuery } from "@tanstack/react-query";
+import { getDocuments } from "utils/query";
+import { useDocumentStore } from "store";
+import { useEffect, useRef } from "react";
 import generateKey from "lib/react/generateKey";
-
-const getDocuments = async () => {
-  const response = await axios.get("/api/document");
-  return response.data;  
-}
 
 type Document = {
   name?: string;
@@ -24,27 +24,33 @@ type Document = {
 
 const Home: NextPage = () => {
   const { status } = useSession();
-  const { data, isLoading } = useQuery(["/api/document"], getDocuments);
+  const documents = useDocumentStore(state => state.queriedDocuments);
   const router = useRouter();
+  const elementToAnimate = useRef(null)
+
+  useEffect(() => {
+    elementToAnimate.current && autoAnimate(elementToAnimate.current)
+  }, [elementToAnimate])
 
   if (status === "unauthenticated") router.push("/auth/signup");
-
-  if (isLoading) return <h1>loading...</h1>;
 
   return (
     <Flex h="100vh" direction="row">
       <Nav />
-      <Flex direction="row" wrap="wrap" gap="8" p="8">
-        <DocumentCard variant="new" />
-        {data.map((document: Document) => (
-          <DocumentCard
-            variant="card"
-            name={document?.name}
-            content={document?.content}
-            documentId={document?._id}
-            key={generateKey(Math.floor(Math.random()*10000))}
-          />
-        ))}
+      <Flex w="100%" direction="column">
+        <Header />
+        <Flex h="100%" wrap="wrap" gap="8" p="8" ref={elementToAnimate}>
+          <DocumentCard variant="new" />
+          {documents === [] || !documents ? "" : documents.map((document: Document) => (
+            <DocumentCard
+              variant="card"
+              name={document?.name}
+              content={document?.content}
+              documentId={document?._id}
+              key={generateKey(Math.floor(Math.random()*10000))}
+            />
+          ))}
+        </Flex>
       </Flex>
     </Flex>
   );
