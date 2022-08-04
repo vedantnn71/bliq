b0f89139a1b666279d807f4e16e11805037f2fd0
Vedant Nandwana <vedantnn7.git@gmail.com>
2022-08-04 19:58:36 +0530

    Update(apps/extension): setup tailwind

--- apps/extension/preferences/src/pages/index.tsx
+++ apps/extension/preferences/src/pages/index.tsx
@@ -1,13 +1,14 @@
 import type { NextPage } from 'next'
-import { ChakraProvider, Box } from "@chakra-ui/react";
-import theme from "ui/theme";
+import Container from "components/layout/container";
 
 const Home: NextPage = () => {
   return (
-    <Box>
-      <h2>Hello world!</h2>
+    <Container>
+      <h1 class="text-3xl font-bold">
+       heyyy world
+      </h1>
       <p>Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.</p>
-    </Box>
+    </Container>
   )
 }
 
