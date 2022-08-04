9eea9dc08d3e1cc60767e2359099635f2d79dc4d
Vedant Nandwana <vedantnn7.git@gmail.com>
2022-08-04 20:13:07 +0530

    Fix(apps/extension): implictly any type for 'Contanier'

--- apps/extension/preferences/src/components/layout/container.tsx
+++ apps/extension/preferences/src/components/layout/container.tsx
@@ -1,4 +1,10 @@
-const Container = ({ children }) => (
+import type { ReactNode } from "react";
+
+interface ContainerProps {
+  children: ReactNode;
+}
+
+const Container = ({ children }: ContainerProps) => (
   <div className="relative flex justify-center w-full">
     <div className="relative overflow-scroll">
       <div className="relative flex flex-col p-1 font-sans font-normal min-w-[420px] w-full">
