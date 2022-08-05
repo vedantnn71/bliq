bf2f5a0b2c21f962dd9c4d2f76024f8c6b069a02
Vedant Nandwana <vedantnn7.git@gmail.com>
2022-08-05 22:16:06 +0530

    Update(extension/preferences): improve ui

--- apps/extension/preferences/src/components/auth.tsx
+++ apps/extension/preferences/src/components/auth.tsx
@@ -5,18 +5,42 @@ const Auth = () => {
   const router = useRouter();
 
   return (
-    <div className="p-6 flex flex-col gap-7">
-      <h1 className="mx-auto font-bold text-2xl text-slate-800">Heyy Welcome! 👋</h1>
+    <div className="py-4 px-6 flex flex-col gap-6 justify-center">
+      <div className="flex flex-row justify-center items-center mx-auto">
+        <img src="/next-assets/logo.svg" className="w-20 mx-0" />
+        <h1 className="font-bold text-3xl text-slate-800">
+          Bliq <span className="text-brand-secondary">for chrome</span>
+        </h1>
+      </div>
+      <div>
+        <h2 className="font-semibold text-slate-800 text-xl">
+          Signup now to:
+        </h2>
+        <ul className="text-lg align-center justify-center py-2 text-gray-800">
+          <li className="flex flex-row gap-1">
+            <img src="/next-assets/check.png" />
+            Get blazing fast auto-completions
+          </li>
+          <li className="flex flex-row gap-1">
+            <img src="/next-assets/check.png" />
+            Increase your productivity
+          </li>
+          <li className="flex flex-row gap-1">
+            <img src="/next-assets/check.png" />
+            Improve your writing
+          </li>
+        </ul>
+      </div>
       <a
-        className="bg-brand-primary p-2 rounded-md text-white text-lg hover:scale-[.98] duration-500 text-center" 
+        className="bg-brand-primary p-2 rounded-md text-white text-lg hover:scale-[.98] duration-500 text-center text-semibold" 
         href={config.appUrl + "/auth/signup"}
         target="_blank"
         rel="noreferrer"
       >
         Signup
-      </a>   
+      </a>  
       <a
-        className="bg-gray-100 secondary p-2 rounded-md text-slate-800 border-2 border-brand-secondary text-lg text-center hover:scale-[.98] duration-500"
+        className="bg-gray-100 secondary p-2 rounded-md text-slate-800 border-2 border-brand-secondary text-lg text-center hover:scale-[.98] duration-500 text-semibold"
         href={config.appUrl + "/auth/login"}
         target="_blank"
         rel="noreferrer"
