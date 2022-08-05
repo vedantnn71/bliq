c855d76c45bf857955888f8b54831510b7f99ea1
Vedant Nandwana <vedantnn7.git@gmail.com>
2022-08-05 22:29:07 +0530

    Implement(extension/preferences): 'isAuthenticated'

---
+++ apps/extension/preferences/src/utils/isAuthenticated.ts
@@ -0,0 +1,16 @@
+const isAuthenticated = async () => {
+  let isSessionToken = false;
+  const cookie = await chrome?.cookies?.get(
+    { url: "https://bliq.vercel.app/", name: "__Secure-next-auth.session-token" },
+  );
+
+  console.log(typeof cookie?.value);
+  
+  if (typeof cookie?.value === "string") {
+    isSessionToken = true;
+  }
+  
+  return isSessionToken;
+}
+
+export default isAuthenticated;
